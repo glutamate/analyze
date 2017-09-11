6acceaea8baa9c4bddb3357f1897f584f3321f7a
Nikita Tchayka <nikitatchayka@gmail.com>
2017-09-11 15:01:27 +0100

    Start from scratch

--- src/Analyze.hs
+++ src/Analyze.hs
@@ -1,14 +1,5 @@
--- | See "Analyze.RFrame".
-module Analyze
-  ( module X
-  ) where
+module Analyze where
 
-import Analyze.Common as X
-import Analyze.Conversions as X
-import Analyze.Csv as X
-import Analyze.Datasets as X
-import Analyze.Decoding as X
-import Analyze.Html as X
-import Analyze.Ops as X
-import Analyze.RFrame as X
-import Analyze.Values as X
+foo :: a
+foo = undefined
+
