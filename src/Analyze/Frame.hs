1a45233ba090c0b16e3ce6fbd1039b4242a2d8a5
Nikita Tchayka <nikitatchayka@gmail.com>
2017-09-16 00:52:08 +0100

    Implement ofColumns

--- src/Analyze/Frame.hs
+++ src/Analyze/Frame.hs
@@ -1,61 +1,29 @@
 module Analyze.Frame where
 
--- | Creates a 'Frame' in the same way as 'Data.Map.fromList'.
--- Series values must be serialized to a 'ByteString' using
--- 'Series.serialize'.
---
--- @
---     myDataFrame :: Frame Text Text
---     myDataFrame = Frame.fromList
---        [ ("Name" , Series.serialize names)
---        , ("Price", Series.serialize prices)
---        ]
--- @
-fromList :: (Serialize key)
-         => [(key, Series key ByteString)]
-         -> AnalyzeOperation (Frame key ByteString)
-fromList = undefined
+import Control.Monad
+import Data.Vector.Mutable as MutableVector
 
+import Analyze.Types
+import Analyze.Series as Series
 
--- | Adds a column to a frame
---
--- @
--- myDataFrame
--- & addColumn "Date" dates
--- & addColumn "Zip code" zipCodes
--- @
-addColumn :: (Serialize key, Serialize value)
-          => key
-          -> Series key value
-          -> Frame key value
-          -> AnalyzeOperation (Frame key value)
-addColumn = undefined
+data Frame rowKey columnKey = Frame
+    { frameKeys    :: IOVector columnKey
+    , frameColumns :: IOVector (Series rowKey)
+    }
 
-
--- | AnalyzeOperation the columns of the 'Frame' as a 'Series'
--- of serialized 'Series' indexed by the same key.
-columns :: (Serialize key, Serialize value)
-        => Frame key value
-        -> AnalyzeOperation (Series key (Series ByteString ByteString))
-columns = undefined
-
-
--- | Total number of columns, including the ones with no value
-countColumns :: (Serialize key, Serialize value)
-             => Frame key value
-             -> AnalyzeOperation Int
-countColumns = undefined
-
-
--- | Total number of rows
-countRows :: (Serialize key, Serialize value)
-          => Frame key value
-          -> AnalyzeOperation Int
-countRows = undefined
-
-
-dropColumn :: (Serialize key, Serialize value)
-           => key
-           -> Frame key value
-           -> AnalyzeOperation (Frame key value)
-dropColumn = undefined
+ofColumns :: (Serializable columnValue)
+          => [ (columnKey, [columnValue] ) ]
+          -> IO (Frame Int columnKey)
+ofColumns keyValues = do
+    keysVector <- MutableVector.new (Prelude.length keyValues)
+    colsVector <- MutableVector.new (Prelude.length keyValues)
+    forM_ (indexed keyValues) $ \(i, (columnKey, column)) -> do
+        colSeries <- Series.ofValues column
+        MutableVector.write keysVector i columnKey
+        MutableVector.write colsVector i colSeries
+    return Frame
+        { frameKeys    = keysVector
+        , frameColumns = colsVector
+        }
+  where
+    indexed = Prelude.zip [0..]
