71bf720554b479d893267da09f8ae64cde02a73c
Nikita Tchayka <nikitatchayka@gmail.com>
2017-10-09 18:31:43 +0100

    Remove Ops module
    It only had the One Hot encoding function, which is more suited for
    a machine learning framework, rather than for a dataframe one.

--- src/Analyze.hs
+++ src/Analyze.hs
@@ -7,6 +7,5 @@ import Analyze.Common as X
 import Analyze.Conversions as X
 import Analyze.Csv as X
 import Analyze.Decoding as X
-import Analyze.Ops as X
 import Analyze.Frame as X
 import Analyze.Values as X
