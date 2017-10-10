1c6ed47d0d5639e9205af9b0adf99024c3993562
Tom Nielsen <tanielsen@gmail.com>
2017-10-10 18:31:46 +0100

    Html: generalise to monad transformer. add thead and tbody

--- src/Analyze/Html.hs
+++ src/Analyze/Html.hs
@@ -6,8 +6,10 @@ import           Control.Monad  (forM_)
 import qualified Lucid          as L
 
 -- | Renders an 'RFrame' to an HTML table.
-renderHtml :: (L.ToHtml k, L.ToHtml v) => RFrame k v -> L.Html ()
+renderHtml :: (L.ToHtml k, L.ToHtml v, Monad m)
+           => RFrame k v -> L.HtmlT m ()
 renderHtml (RFrame ks _ vs) =
   L.table_ $ do
-    L.tr_ $ forM_ ks (L.th_ . L.toHtml)
-    forM_ vs $ \v -> L.tr_ (forM_ v (L.td_ . L.toHtml))
+    L.thead_ $
+      L.tr_ $ forM_ ks (L.th_ . L.toHtml)
+    L.tbody_ $ forM_ vs $ \v -> L.tr_ (forM_ v (L.td_ . L.toHtml))
