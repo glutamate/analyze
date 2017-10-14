fb1a162eecfb2c6286399963dc2e326bd6246715
Tom Nielsen <tanielsen@gmail.com>
2017-10-14 11:31:57 +0100

    filter

--- src/Analyze/New.hs
+++ src/Analyze/New.hs
@@ -8,6 +8,18 @@ import           Data.HashMap.Strict (HashMap)
 import qualified Data.HashMap.Strict as HM
 import Data.Hashable
 
+class (Functor f, Foldable f) => FrameContainer f where
+  zipWithFC :: (a->b->c) -> f a -> f b -> f c
+  mzipWithFC :: (a->b->Maybe c) -> f a -> f b -> f c
+
+instance FrameContainer [] where
+  zipWithFC = zipWith
+  mzipWithFC f [] _ = []
+  mzipWithFC f _ [] = []
+  mzipWithFC f (x:xs) (y:ys) = case f x y of
+    Nothing -> mzipWithFC f xs ys
+    Just z -> z:mzipWithFC f xs ys
+
 data Column f where
   Col :: Typeable a => f a -> Column f
 
@@ -26,6 +38,16 @@ mapCol k f fr@(Frame hm) =
       nc = Col $ fmap f c
   in Frame $ HM.insert k nc hm
 
+filterCol :: (Eq k, Hashable k, Typeable a, Show k, FrameContainer f)
+  => k -> (a -> Bool) -> Frame f k -> Frame f k
+filterCol k f fr@(Frame hm) =
+  let c = col k fr
+      trues = fmap f c
+      w True x = Just x
+      w False _ = Nothing
+  in Frame $ HM.map (\(Col g) -> Col $ mzipWithFC w trues g) hm
+
+
 col :: (Eq k, Hashable k, Typeable a, Show k, Functor f)
     => k -> Frame f k -> f a
 col k fr = case HM.lookup k (frameData fr) of
@@ -41,7 +63,7 @@ myCol :: [Int]
 myCol = col "b" myFrame
 
 myCol1 :: [Int]
-myCol1 = col "b" $ mapCol "b" (+(1::Int)) myFrame
+myCol1 = col "b" $ mapCol "b" (+(1::Int)) $ filterCol "b" (odd::Int -> Bool) myFrame
 
 {- todo
 
